c9a70c93c78f61821d633a3c18ef70c02bdc227f
MarkyMan4 <markus.hutnik@gmail.com>
2024-04-07 22:06:17 -0500

    changed Convert method to Parse which returns an HtmlBody that can be rendered

--- md2html.go
+++ md2html.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+var (
+	h1Regex = regexp.MustCompile("^# ")
+)
+
 type HtmlElement struct {
 	XMLName xml.Name
 	Id      string `xml:"id,attr,omitempty"`
@@ -22,49 +26,35 @@ func NewHtmlElement(tagName string, content string, id string) *HtmlElement {
 	return elem
 }
 
-type HtmlDoc struct {
-	XMLName xml.Name `xml:"html"`
-	Head    *HtmlHead
-	Body    *HtmlBody
-}
-
-type HtmlHead struct {
-	XMLName xml.Name `xml:"head"`
-	Title   string   `xml:"title"`
-}
 type HtmlBody struct {
-	XMLName  xml.Name `xml:"body"`
 	Elements []*HtmlElement
 }
 
-func NewHtmlDoc(title string) *HtmlDoc {
-	return &HtmlDoc{
-		Head: &HtmlHead{Title: title},
-		Body: &HtmlBody{Elements: []*HtmlElement{}},
+func NewHtmlBody() *HtmlBody {
+	return &HtmlBody{
+		Elements: []*HtmlElement{},
 	}
 }
 
-func (hd *HtmlDoc) Render() string {
-	rendered := "<!DOCTYPE html>\n"
-
-	res, err := xml.MarshalIndent(hd, "", "    ")
+func (hb *HtmlBody) Render() string {
+	res, err := xml.MarshalIndent(hb.Elements, "", "    ")
 	if err != nil {
 		panic("failed to render HTML document " + err.Error())
 	}
 
-	return rendered + string(res)
+	return string(res)
 }
 
-func Convert(md string) string {
-	htmlDoc := NewHtmlDoc("Markdown")
+func ParseMarkdown(md string) *HtmlBody {
+	html := NewHtmlBody()
 
 	lines := strings.Split(md, "\n")
 	for _, line := range lines {
-		if matched, _ := regexp.Match("^# ", []byte(line)); matched {
+		if matched := h1Regex.Match([]byte(line)); matched {
 			h1 := NewHtmlElement("h1", line, "")
-			htmlDoc.Body.Elements = append(htmlDoc.Body.Elements, h1)
+			html.Elements = append(html.Elements, h1)
 		}
 	}
 
-	return htmlDoc.Render()
+	return html
 }
--- md2html_test.go
+++ md2html_test.go
@@ -7,7 +7,11 @@ import (
 	"github.com/MarkyMan4/md2html"
 )
 
+const md = `# hello
+# this is a test
+`
+
 func TestConvert(t *testing.T) {
-	res := md2html.Convert("# hello")
-	fmt.Println(res)
+	res := md2html.ParseMarkdown(md)
+	fmt.Println(res.Render())
 }
